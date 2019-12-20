e3a99238e2aaee16214b52fde6c4ab7761ed4e50
Ayako.Ito <komo0925@gmail.com>
2019-12-20 03:21:49 +0000

    タスクリストの完成（第一課題まで）

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,30 @@
 
 @section('content')
 
-<!-- ここにページ毎のコンテンツを書く -->
+    <h1>タスク一覧</h1>
+    
+    @if(count($tasks) > 0)
+    
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <td>タスク</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+            
+        </table>
+        
+        {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-success']) !!}
+        
+    @endif
 
 @endsection

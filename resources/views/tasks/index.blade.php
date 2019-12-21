340059583f50db94ddd140794fe36278faf5c1c6
Ayako.Ito <komo0925@gmail.com>
2019-12-21 07:41:59 +0000

    バリデーション、カラム（status）の追加

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -11,6 +11,7 @@
                 <tr>
                     <th>id</th>
                     <td>タスク</td>
+                    <td>ステータス</td>
                 </tr>
             </thead>
             <tbody>
@@ -18,6 +19,7 @@
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
+                    <td>{{ $task->status }}</td>
                 </tr>
                 @endforeach
             </tbody>
